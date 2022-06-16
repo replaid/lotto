0d3f465ddbf6b1a82ba8bbabfd293ebf2e0d8280
John Platte <john.platte@rosieapp.com>
2022-06-16 11:47:44 +0300

    Implement all known cases for #score_line.

--- score.rb
+++ score.rb
@@ -1,9 +1,61 @@
 class Score
 
+  EMPTY = {}
+
+  ONE_PAIR = {2=>1}
+
+  TWO_PAIRS = {2=>2}
+
+  MAYBE_SEQUENCE = {1=>5}
+
+  FULL_HOUSE = {3=>1, 2=>1}
+
+  TRIPLET = {3=>1}
+
+  QUARTET = {4=>1}
+
   def score_line(line)
-    sum = 0
+    # Eliminate nils, then count occurrences of each value.
     tally = line.compact.tally
-    sum += 10 if tally.values.any? { 2 }
-    sum
+
+    # Now count occurrences of singles, doubles, triplets, and quartets.
+    tally_tally = tally.values.tally
+
+    # Remove singles counts to decomplicate our logic, since we only look at
+    # them in one case.
+    group_tally = tally_tally.dup.delete_if { |group_size, _occurrences| group_size == 1 }
+
+    # Now return a score based mostly on which pattern the group tally matches.
+    case group_tally
+    when EMPTY
+      # This is the only wrinkle left: we have to recognize sequences, which
+      # look like 5 singles in the tally and were removed in the
+      # simplification, so look "empty" to the group tally.
+      #
+      # Then once we know we have 5 singles, we can just check that they span 5
+      # consecutive integers to see if we have a sequence or not.
+      if (tally_tally == MAYBE_SEQUENCE) && is_sequence?(line)
+        25
+      else
+        0
+      end
+    when ONE_PAIR
+      10
+    when TWO_PAIRS
+      20
+    when TRIPLET
+      30
+    when QUARTET
+      50
+    when FULL_HOUSE
+      40
+    else
+      raise 'unrecognized case: ' + tally_tally.inspect
+    end
+  end
+
+  def is_sequence?(line)
+    sorted = line.compact.sort
+    (sorted.first.to_i + 4) == sorted.last
   end
 end
--- score_test.rb
+++ score_test.rb
@@ -4,13 +4,67 @@ require 'minitest/autorun'
 require_relative 'score'
 
 class ScoreTest < Minitest::Test
-  def test_can_score_line_of_nils
-    score = Score.new
-    assert_equal(0, score.score_line([nil, nil, nil, nil, nil]))
+  def setup
+    @score = Score.new
   end
 
-  def test_can_score_line_with_nils_and_one_pair
-    score = Score.new
-    assert_equal(10, score.score_line([nil, nil, nil, 1, 1]))
+  def test_can_score_all_singles
+    [
+      [nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, 1],
+      [nil, nil, nil, 1, 2],
+      [nil, nil, 3, 1, 2],
+      [nil, 4, 3, 1, 2],
+      [12, 4, 3, 2, 1]
+    ].each do |line|
+      assert_equal(0, @score.score_line(line))
+    end
+  end
+
+  def test_can_score_one_pair
+    [
+      [nil, nil, nil, 1, 1],
+      [nil, nil, 2, 1, 1],
+      [nil, 3, 2, 1, 1],
+      [4, 3, 2, 1, 1],
+    ].each do |line|
+      assert_equal(10, @score.score_line(line))
+    end
+  end
+
+  def test_can_score_two_pairs
+    [
+      [nil, 2, 2, 1, 1],
+      [3, 2, 2, 1, 1]
+    ].each do |line|
+      assert_equal(20, @score.score_line(line))
+    end
+  end
+  def test_can_score_triplet
+    [
+      [nil, nil, 1, 1, 1],
+      [nil, 2, 1, 1, 1],
+      [3, 2, 1, 1, 1]
+    ].each do |line|
+      assert_equal(30, @score.score_line(line))
+    end
+  end
+
+  def test_can_score_quartet
+    [
+      [nil, 1, 1, 1, 1],
+      [2, 1, 1, 1, 1]
+    ].each do |line|
+      assert_equal(50, @score.score_line(line))
+    end
+  end
+
+  def test_can_score_full_house
+    assert_equal(40, @score.score_line([1,1,1,2,2]))
+  end
+
+  def test_can_score_sequence
+    assert_equal(25, @score.score_line([1,2,3,4,5]))
+    assert_equal(0, @score.score_line([1,2,3,4,12]))
   end
 end
