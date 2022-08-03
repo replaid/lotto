1e51215db38d648c946bd65946d8c6097db81a8e
John Platte <john.platte@rosieapp.com>
2022-08-03 23:17:22 +0300

    Extract NaiveScoreStrategy#loneliest.

--- strategy/naive_score_strategy.rb
+++ strategy/naive_score_strategy.rb
@@ -12,11 +12,7 @@ class NaiveScoreStrategy
   #   so we can maximize distribution, and grab one of the spaces that shares
   #   the highest loneliness score.
   def place(board, new_number)
-    lonelinesses = high_scoring_spaces(board, new_number).map do |x,y|
-      [x,y,board.loneliness(x,y)]
-    end
-    max_loneliness = lonelinesses.map(&:last).max
-    lonelinesses.detect {|x,y,loneliness| loneliness == max_loneliness}.first(2)
+    loneliest(board, high_scoring_spaces(board, new_number))
   end
 
   def high_scoring_spaces(board, new_number)
@@ -30,5 +26,13 @@ class NaiveScoreStrategy
     grouped_scores = scored_things.group_by {|x,y,new_score| new_score}
     high_score = grouped_scores.keys.sort.last
     grouped_scores[high_score].map {|x,y,_score| [x,y] }
+  end
+
+  def loneliest(board, spaces)
+    lonelinesses = spaces.map do |x,y|
+      [x,y,board.loneliness(x,y)]
+    end
+    max_loneliness = lonelinesses.map(&:last).max
+    lonelinesses.detect {|x,y,loneliness| loneliness == max_loneliness}.first(2)
   end
 end
